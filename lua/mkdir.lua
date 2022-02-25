f88d42e5124527a394945920c2684d7935f288d8
jghauser <julian@julianhauser.com>
2022-02-25 18:09:28 +0100

    fixes #5; create dirs in path file is saved to

--- lua/mkdir.lua
+++ lua/mkdir.lua
@@ -8,13 +8,14 @@ local cmd = vim.cmd
 local M = {}
 
 function M.run()
-  local dir = fn.expand('%:p:h')
+  local dir = fn.expand('<afile>:p:h')
 
   if fn.isdirectory(dir) == 0 then
     fn.mkdir(dir, 'p')
   end
 end
 
+cmd([[autocmd!]])
 cmd([[autocmd BufWritePre * lua require('mkdir').run()]])
 
 return M
